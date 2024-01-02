6482d8c26c284a3e9cf9d2d1b0c3407cea4e1ef1
Diwakar <diwakar.singh313@gmail.com>
2024-01-02 16:33:04 +0530

    added login api

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import http from "http";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
 import { buildSchema } from "type-graphql";
 import { AppDataSource } from "./data-source";
 import { UserResolver } from "./resolvers/user.resolver";
@@ -9,6 +11,7 @@ const PORT = process.env.PORT || 8080;
 
 (async () => {
   const app = express();
+  const httpServer = http.createServer(app);
 
   await AppDataSource.initialize();
 
@@ -16,20 +19,15 @@ const PORT = process.env.PORT || 8080;
     schema: await buildSchema({
       resolvers: [UserResolver],
     }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
   await apolloServer.start();
 
-  app.use(
-    "/graphql",
-    express.json(),
-    expressMiddleware(apolloServer, {
-      context: async ({ req }) => ({ token: req.headers.token }),
-    })
-  );
+  app.use("/graphql", express.json(), expressMiddleware(apolloServer));
 
   app.get("/", (_req, res) => res.send("<h1>JWT GraphQL Auth Server!</h1>"));
 
-  app.listen(PORT, () => {
+  httpServer.listen(PORT, () => {
     console.log("Server started on port", PORT);
   });
 })();
--- server/src/resolvers/user.resolver.ts
+++ server/src/resolvers/user.resolver.ts
@@ -1,12 +1,48 @@
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
-import { hash } from "bcryptjs";
+import {
+  Arg,
+  Field,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+} from "type-graphql";
+import { compare, hash } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 import { User } from "../entity/User";
+
+const JWT_SECRET = "ps6lzFSyYP7e8a6GJCsXtANTQefw0s1O";
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
 
 @Resolver()
 export class UserResolver {
   @Query(() => [User])
   users() {
     return User.find();
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error("User not found.");
+    }
+
+    const isValidPassword = await compare(password, user.password);
+
+    if (!isValidPassword) throw new Error("Invalid credentials.");
+
+    return {
+      accessToken: sign({ userId: user.id }, JWT_SECRET, { expiresIn: "1h" }),
+    };
   }
 
   @Mutation(() => Boolean)
